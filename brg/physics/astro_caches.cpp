abefc7b03ca4d237cf9debf9ce1559e8a531bd58
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2014-09-02 16:42:40 +0100

    Restoring pt 8

--- brg/physics/astro_caches.cpp
+++ brg/physics/astro_caches.cpp
@@ -28,6 +28,7 @@
 #include "brg/global.h"
 
 #include "brg/math/cache/cache.hpp"
+#include "brg/math/cache/cache_2d.hpp"
 
 #include "brg/physics/astro.h"
 #include "brg/physics/units/unit_obj.h"
@@ -39,6 +40,10 @@
 
 // Initialisation for brgastro::dfa_cache
 DEFINE_BRG_CACHE_STATIC_VARS( dfa_cache, 0, 5, 0.01 );
+
+// Initialisation for brgastro::add_cache
+DEFINE_BRG_CACHE_2D_STATIC_VARS( add_cache, 0, 5, 0.01,
+		                                    0, 5, 0.01);
 
 // Initialisation for brgastro::tfa_cache
 DEFINE_BRG_CACHE_STATIC_VARS( tfa_cache, 0.001, 1.02, 0.001 );
@@ -56,6 +61,12 @@ const double brgastro::dfa_cache::_calculate( const double in_params ) const
 }
 
 #endif // end brgastro::dfa_cache functions
+
+// brgastro::add_cache class methods
+const double brgastro::add_cache::_calculate( const double in_param_1, const double in_param_2) const
+{
+	return brgastro::integrate_add(in_param_1,in_param_2);
+}
 
 const double brgastro::tfa_cache::_calculate( const double in_params ) const
 {
