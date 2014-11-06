36acfe0dcf4bd02a29562015900de878b6742b26
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2014-11-06 17:52:04 +0000

    Some work on implementing labeled_matrix.

---
+++ brg/container/labeled_matrix.hpp
@@ -0,0 +1,33 @@
+/**********************************************************************\
+ @file labeled_matrix.hpp
+ ------------------
+
+ TODO <Insert file description here>
+
+ **********************************************************************
+
+ Copyright (C) 2014  Bryan R. Gillis
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+\**********************************************************************/
+
+#ifndef _BRG_CONTAINER_LABELED_MATRIX_HPP_INCLUDED_
+#define _BRG_CONTAINER_LABELED_MATRIX_HPP_INCLUDED_
+
+
+
+
+
+#endif // _BRG_CONTAINER_LABELED_MATRIX_HPP_INCLUDED_
